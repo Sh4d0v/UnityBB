13a2c4b36add94db1bc4d023193f958c33c118f4
Sh4d0v <qbakuzma@gmail.com>
2013-07-14 22:52:22 +0200

    index.php commit

---
+++ index.php
@@ -0,0 +1,13 @@
+﻿<?php
+/**
+ * UnityBB
+ * beta v0.1
+ *
+ * @author 		Jakub Kuźma, Paweł Kobel
+ * @copyright	(c) 2013 Jakub Kuźma, Paweł Kobel.
+ *
+ */
+
+	require_once('unity/init.php');
+	
+?>
